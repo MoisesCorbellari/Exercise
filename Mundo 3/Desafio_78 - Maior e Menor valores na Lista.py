7714975c8e9275cbf3de0542b352d1ea957a4988
Moises Corbellari <moisescorbellari1@outlook.com>
2024-01-05 01:59:18 -0300

    Desafios

--- Mundo 3/Desafio_78 - Maior e Menor valores na Lista.py
+++ Mundo 3/Desafio_78 - Maior e Menor valores na Lista.py
@@ -1,6 +1,6 @@
 ''' Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. 
 No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista. '''
-valor = []
+valor = list()
 for p in range (5):
     pos = int(input(f'Digite o {p + 1}º valor: '))
     valor.append(pos)
