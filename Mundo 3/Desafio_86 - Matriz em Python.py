f5041c75fadb5d2d4c1c9ea1b7d20098f4d96b62
Moises Corbellari <moisescorbellari1@outlook.com>
2024-02-18 01:41:55 -0300

    Desafios

--- Mundo 3/Desafio_86 - Matriz em Python.py
+++ Mundo 3/Desafio_86 - Matriz em Python.py
@@ -3,9 +3,9 @@ Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores 
 No final, mostre a matriz na tela, com a formatação correta.
 '''
 matriz = []
-for l in range(0,3):
+for l in range(3):
     linha = []
-    for c in range(0,3):
+    for c in range(3):
         valor = int(input(f'Digite valores para [{l},{c}]:'))
         linha.append(valor)
     matriz.append(linha)
@@ -13,8 +13,8 @@ print('='*40)
 print('''
         MATRIZ 3x3:
 ''')
-for l in range(0,3):
-    for c in range(0,3):
+for l in range(3):
+    for c in range(3):
         print(f'[{matriz[l][c]:^5}]', end='')
     print()
 print('='*40)
