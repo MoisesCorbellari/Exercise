66ae9515512c371499bd8322c06947118cfd5731
Moises Corbellari <moisescorbellari1@outlook.com>
2024-04-12 00:27:23 -0300

    Desafios

--- Mundo 3/Desafio_90 - Dicionário em Python.py
+++ Mundo 3/Desafio_90 - Dicionário em Python.py
@@ -2,10 +2,13 @@
 guardando também a situação em um dicionário. 
 No final, mostre o conteúdo da estrutura na tela.'''
 
+from time import sleep
 alunos = {}
 while True:
     nome = input('Digite o nome do aluno (ou digite "sair" para encerrar o programa): ').capitalize()
     if nome.lower() == 'sair':
+        print('\nPrograma encerrado!\n')
+        sleep(1)
         break
     media = float(input('Informe a média do aluno: '))
     if media >= 7:
@@ -16,6 +19,6 @@ while True:
         situacao = 'Reprovado!'
     alunos[nome] = {'Média': media, 'Situação': situacao}
 
-print('-'*10 + "Listagem dos Alunos" + '-'*10)
+print(' '*10 + " Listagem dos Alunos: \n")
 for aluno, info in alunos.items():
-    print(f'Nome: {aluno} | Média: {info["Média"]} | Situação: {info["Situação"]}')
+    print(f'Nome: {aluno} | Média: {info["Média"]} | Situação: {info["Situação"]}\n')
