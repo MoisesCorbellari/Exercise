4b115ae6e9d98fb97abf3711ec46287b409fca6a
Moises Corbellari <moisescorbellari1@outlook.com>
2024-04-19 03:03:23 -0300

    Desafios

--- Mundo 3/Desafio_94 - Unindo dicionários e listas.py
+++ Mundo 3/Desafio_94 - Unindo dicionários e listas.py
@@ -5,17 +5,18 @@ A) Quantas pessoas foram cadastradas
 B) A média de idade 
 C) Uma lista com as mulheres 
 D) Uma lista de pessoas com idade acima da média'''
+
+pessoa = dict()
 dados = list()
-soma = 0
 while True:
-    pessoa = dict()
+    pessoa.clear()
     pessoa['nome'] = str(input('Nome: ')).capitalize().strip()
-    pessoa['sexo'] = str(input('Informe o sexo [M/F]: ')).upper().strip()
+    pessoa['sexo'] = str(input('Informe o sexo [M/F]: ')).upper()[0].strip()
     pessoa['idade'] = int(input('Idade: '))
     while pessoa['sexo'] not in 'MF':
         print('ERRO! Por favor, digite apenas M ou F.')
+        pessoa['sexo'] = str(input('Informe o sexo [M/F]: ')).upper()[0].strip()
         print()
-        pessoa['sexo'] = str(input('Informe o sexo [M/F]: ')).upper().strip()
     dados.append(pessoa.copy())
     resp = str(input('Deseja continuar? [S/N]: '))
     if resp in 'Nn':
@@ -23,15 +24,18 @@ while True:
 print('-=' * 30)
 print(f'A) Quantas pessoas foram cadastradas? {len(dados)}')
 soma_idade = sum(pessoa['idade'] for pessoa in dados)
-media_idade = soma_idade / len(dados)
-print(f'B) A média de idade: {media_idade:.2f} anos')
+media = soma_idade / len(dados)
+print(f'B) A média de idade: {media:.2f} anos')
 print('C) Uma lista com as mulheres:')
 for pessoa in dados:
-    if pessoa['sexo'] == 'F':
-        print(pessoa['nome'])
+    if pessoa['sexo'] in 'F':
+        print(f"{pessoa['nome']}")
 print('D) Uma lista de pessoas com idade acima da média:')
 for pessoa in dados:
-    if pessoa['idade'] > media_idade:
-        print(pessoa['nome'])
+    if pessoa['idade'] >= media:
+        print('     ', end='')
+        for k, v in pessoa.items():
+            print(f'{k} = {v}; ', end='')
+        print()
 print('-=' * 30)
 print(f'{"< ENCERRADO >":^60}')
