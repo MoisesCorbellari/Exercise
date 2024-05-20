bfc6e5eedf66ed4e68c8030f0bcd9b557d341d10
Moises Corbellari <moisescorbellari1@outlook.com>
2024-05-19 22:56:48 -0300

    Desafio

--- Mundo 3/Desafio_101 - Funções para votação.py
+++ Mundo 3/Desafio_101 - Funções para votação.py
@@ -5,52 +5,54 @@ retornando um valor literal indicando se uma pessoa tem voto
 NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 '''
 
-from datetime import date # importa a função date para trabalhar com datas
-from time import sleep # importa a função sleep para criar um atraso na animação de carregamento
-from progress.spinner import MoonSpinner # importa a classe MoonSpinner para criar uma animação de carregamento
+# Importação de libs para trabalhar com data, atraso e carregamento de animação
+from datetime import date
+from time import sleep
+from progress.spinner import MoonSpinner
 
 def linha():
-    print('-'*80) # imprime uma linha com 80 hifens
+    print('-'*80)
 
 def title(txt):
-    msg = len(txt) + 50 # calcula o tamanho total da mensagem
-    print(f'{txt:>40}') # centraliza o texto com padding à direita
-    print('-'*msg) # imprime linha de hifens do tamanho da mensagem
+    msg = len(txt) + 50
+    print(f'{txt:>40}')
+    print('-'*msg)
 
-def voto(ano): # recebe o ano de nascimento como entrada
-    idade = date.today().year - ano # calcula a idade da pessoa subtraindo o ano de nascimento do ano atual
-    # utiliza às instruções condicionais para determinar a situação do voto com base na idade:
+def voto(ano):
+    """ Determina a situação do voto com base no ano de nascimento """
+    idade = date.today().year - ano
     if 16 <= idade < 18 or idade > 65:
         return f' com {idade} anos seu voto é OPCIONAL.'
     elif idade < 16:
         return f'com {idade} anos você ainda NÃO POSSUI idade para votar.'
     else:
         return f'com {idade} anos seu voto é OBRIGATÓRIO.'
-# main program
+
 title('SISTEMA DE VERIFICAÇÃO DE VOTO')
 
 while True:
-    nome = input('\nInforme seu nome: ').strip().capitalize() # pede o nome, remove os espaços e deixa a primeira letra maiúscula 
-    while True: # entra em um loop interno para validar o ano de nascimento
+    nome = input('\nInforme seu nome: ').strip().capitalize() 
+    # Loop interno para validar o ano de nascimento
+    while True:
         try:
-            ano = int(input('Ano de nascimento: ')) # pede o ano de nascimento do usuário
+            ano = int(input('Ano de nascimento: '))
             ano_atual = date.today().year
             if ano > ano_atual or ano < 1900:
-                print('\nAno inválido. Tente novamente.\n') # se digitar um ano menor que 1900, vai dar erro e pedir para digitar o ano novamente
+                print('\nAno inválido. Tente novamente.\n')
                 continue
             break
         except ValueError:
-            print('\nEntrada inválida. Tente novamente.\n') # se apertar 'enter' sem digitar o ano de nascimento, vai dar erro e pedir para digitar o ano
+            print('\nEntrada inválida. Tente novamente.\n')
             
-    with MoonSpinner('Processando informação ... ') as bar: # animação de carregamento 
+    with MoonSpinner('Processando informação ... ') as bar:
         for _ in range(50):
             sleep(0.04)
             bar.next()
 
-    print(f'\n{nome}, {voto(ano)}') #informa o nome, a idade e situação do voto
+    print(f'\n{nome}, {voto(ano)}')
     resp = input('\nDeseja sair? ("S" para sim, e "N" para não.): ').strip().upper()
     linha()
     if resp == 'S':
-        print(f'\n{"Volte sempre! ...":>40}\n') # caso a resposta seja "sim (S)", o programa encerra exibindo uma mensagem de despedida
+        print(f'\n{"Volte sempre! ...":>40}\n')
         sleep(1)
         break
