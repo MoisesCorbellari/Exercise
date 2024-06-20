e5e1305e4290ca904b2de9f4e661f254f0bc1372
Moises Corbellari <moisescorbellari1@outlook.com>
2024-06-20 01:56:02 -0300

    desafio

---
+++ Mundo 3/Desafio_109/inf0.py
@@ -0,0 +1,26 @@
+import moeda
+
+moeda.title('Analise de valor\n')
+
+while True:
+    try:
+        p = float(input('Digite um valor: R$'))
+        break
+    except ValueError:
+        print('\033[31mEntrada inválido! Digite um número.\033[0m\n')
+while True:
+    try:
+        taxa = float(input('Informe a taxa (em porcentagem " % "): '))
+        if taxa >= 0:
+            break
+        else:
+            print('\033[31mTaxa não pode ser negativa!\033[0m\n')
+    except ValueError:
+        print('\033[31mEntrada inválido! Digite um número.\033[0m\n')
+print()
+moeda.title(f'Valor analisado: {moeda.coin(p)}\n')
+
+print(f'-> O dobro de {moeda.coin(p)} é {moeda.dobro(p, True)}')
+print(f'-> A metade de {moeda.coin(p)}, é {moeda.metade(p, True)}')
+print(f'-> Com aumento de {moeda.tx(taxa)} sobre o valor de {moeda.coin(p)}, fica em {moeda.aumento(p, taxa, True)}')
+print(f'-> Com a diminuição de {moeda.tx(taxa)} sobre o valor de {moeda.coin(p)}, fica em {moeda.diminuir(p, taxa, True)}\n')
---
+++ Mundo 3/Desafio_109/moeda.py
@@ -0,0 +1,72 @@
+def aumento(preco=0, taxa=0, formato=False):
+    """
+    Calcula o aumento de um preço com base em uma taxa percentual.
+
+    :param preco - valor inicial do preço
+    :param taxa - taxa de aumento em porcentagem
+    :param formato - se True, retorna o valor formatado como moeda
+    :return - valor com aumento aplicado, formatado ou não
+    """
+    calc = preco + (preco * taxa/100)
+    return calc if not formato else coin(calc)
+
+def diminuir(preco=0, taxa=0, formato=False):
+    """
+    Calcula a diminuição de um preço com base em uma taxa percentual.
+
+    :param preco - valor inicial do preço
+    :param taxa - taxa de diminuição em porcentagem
+    :param formato - se True, retorna o valor formatado como moeda
+    :return - valor com a diminuição aplicada, formatado ou não
+    """
+    calc = preco - (preco * taxa/100)
+    return calc if not formato else coin(calc)
+
+def dobro(preco=0, formato=False):
+    """
+    Calcula o dobro de um preço.
+
+    :param preco - valor inicial do preço.
+    :param formato - se True, retorna o valor formatado como moeda.
+    :return - o dobro do valor, formatado ou não.
+    """
+    calc = preco * 2
+    return calc if not formato else coin(calc)
+
+def metade(preco=0, formato=False):
+    """
+    Calcula a metade de um preço.
+
+    :param preco - valor inicial do preço.
+    :param formato - se True, retorna o valor formatado como moeda.
+    :return - a metade do valor, formatado ou não.
+    """
+    calc = preco / 2
+    return calc if not formato else coin(calc)
+
+def coin(preco=0, coin='R$'):
+    """
+    Formata um valor numérico como moeda.
+
+    :param preco - valor a ser formatado.
+    :param coin - símbolo da moeda.
+    :return - valor formatado como moeda.
+    """
+    return f'{coin}{preco:.2f}'.replace('.', ',')
+
+def tx(taxa=0):
+    """
+    Formata uma taxa percentual.
+
+    :param taxa: valor da taxa a ser formatada.
+    :return - taxa formatada.
+    """
+    return f'{taxa:.2f}%'.replace('.', ',')
+
+def title(txt):
+    """
+    Imprime um título centralizado.
+
+    :param txt: texto do título
+    """
+    print(f'{txt:^70}')
