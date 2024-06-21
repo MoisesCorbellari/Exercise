8d03b4df05420083891f9a0cda3c1e8481ef19ea
Moises Corbellari <moisescorbellari1@outlook.com>
2024-06-21 03:29:01 -0300

    desafio

---
+++ Mundo 3/Desafio_110/inf.py
@@ -0,0 +1,19 @@
+import moeda
+
+moeda.title('Analisando valores\n')
+while True:
+    try:
+        p = float(input('Digite o preço: R$'))
+        break
+    except ValueError:
+        print('\033[31mEntrada inválida! Informe um valor\033[0m\n')
+while True:
+    try:
+        tx = float(input('Informe a taxa (em porcentagem " % "): '))
+        if tx >= 0:
+            break
+        else:
+            print('\033[31mTaxa não pode ser negativa!\033[0m\n')
+    except ValueError:
+        print('\033[31mEntrada inválida! Informe um valor\033[0m\n')
+moeda.resumo(p, tx)
