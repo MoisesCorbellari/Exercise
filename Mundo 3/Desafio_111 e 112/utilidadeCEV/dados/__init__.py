f07553fa99c00e56b741b295a5dca564f1e1d903
Moises Corbellari <moisescorbellari1@outlook.com>
2024-06-24 02:12:45 -0300

    update

--- Mundo 3/Desafio_111 e 112/utilidadeCEV/dados/__init__.py
+++ Mundo 3/Desafio_111 e 112/utilidadeCEV/dados/__init__.py
@@ -1,9 +1,11 @@
 def leiaDinheiro(prompt):
     valido = False
     while not valido:
-        entrada = input(prompt).strip()
-        if entrada.isalpha() or entrada == '':
-            print(f'\033[31mO valor \"{entrada}\" é inválido!\033[0m\n')
-        else:
+        entrada = input(prompt).strip().replace('.', '').replace(',', '.').replace(',', '')
+        try:
+            valor = float(entrada)
             valido = True
-            return float(entrada)
+            return valor
+        except ValueError:
+            print(f'\033[31mO valor \"{entrada}\" é inválido!\033[0m\n')
+            
