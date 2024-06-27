7275ea4cb8f701e94940483a73e153acc9669524
Moises Corbellari <moisescorbellari1@outlook.com>
2024-06-27 10:36:49 -0300

    desafio url

---
+++ Mundo 3/Desafio_114 - Site está acessível.py
@@ -0,0 +1,18 @@
+'''
+Crie um código em Python que teste 
+se o site pudim está acessível pelo computador usado.
+'''
+
+import urllib
+import urllib.error
+import urllib.request
+
+try:
+    site = 'http://www.pudim.com.br/'
+    response = urllib.request.urlopen(site)
+    print(f'\n\033[92m{" Conectado com sucesso ":^50}\033[0m')
+    print(f'\033[92mUrl: {site}\033[0m\n'.center(60))
+
+except urllib.error.URLError:
+    print(f'\n\033[31m{" Não foi possível acessar o site ":^50}\033[0m')
+    print(f'\033[31mUrl: {site}\033[0m\n'.center(60))
