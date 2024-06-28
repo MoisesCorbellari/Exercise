1044e77b39aa82e9db3b1556d6e82ba1d05519e6
Moises Corbellari <moisescorbellari1@outlook.com>
2024-06-27 22:12:26 -0300

    update

--- Mundo 3/Desafio_114 - Site está acessível.py
+++ Mundo 3/Desafio_114 - Site está acessível.py
@@ -11,8 +11,10 @@ try:
     site = 'http://www.pudim.com.br/'
     response = urllib.request.urlopen(site)
     print(f'\n\033[92m{" Conectado com sucesso ":^50}\033[0m')
+    print(f'\033[92m{" Status code: 200 ":^50}\033[0m')
     print(f'\033[92mUrl: {site}\033[0m\n'.center(60))
 
 except urllib.error.URLError:
     print(f'\n\033[31m{" Não foi possível acessar o site ":^50}\033[0m')
+    print(f'\033[31m{" Status code: 404 ":^50}\033[0m')
     print(f'\033[31mUrl: {site}\033[0m\n'.center(60))
